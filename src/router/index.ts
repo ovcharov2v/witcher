aaab50ed32a7101bb9b027624a67217c9b7f5c63
Viktor Ovcharov <ovcharov.2v@gmail.com>
2022-05-16 05:07:02 +0400

    Set page titles

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,18 +6,29 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home page",
+    },
   },
   {
     path: "/:path(.*)",
     name: "404",
     component: () =>
       import(/* webpackChunkName: "404" */ "@/views/NotFound.vue"),
+    meta: {
+      title: "Not found4", // <-- Find how to insert :userId into page title
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta!.title}`;
+  next();
 });
 
 export default router;
